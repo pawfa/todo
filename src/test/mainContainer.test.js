9e450923a0cdccfc68eb25a3e94dcd0c7373021b
Pawfa <fajfer.pawel@gmail.com>
2018-07-14 14:29:35 +0200

    Refactoring tests.

--- src/test/mainContainer.test.js
+++ src/test/mainContainer.test.js
@@ -3,21 +3,41 @@ import {shallow, mount, render} from 'enzyme';
 import MainContainer from '../Containers/MainContainer';
 import {ButtonsComponent} from '../Components/Buttons';
 
+function setup() {
+
+  const mainContainer = mount(
+      <MainContainer
+          data={[]}
+      />,
+  );
+
+  mainContainer.find(ButtonsComponent).find('[value="Add"]').simulate('click');
+  mainContainer.find(ButtonsComponent).find('[value="Add"]').simulate('click');
+
+  return {
+    main: mainContainer,
+  };
+
+}
+
 describe('MainContainer', () => {
+  const {main} = setup();
 
-  test('displays text based on props.content', () => {
-    const main = mount(
-        <MainContainer
-            data={[]}
-        />,
-    );
+  test('clicking add button is increasing data length by one', () => {
 
     main.find(ButtonsComponent).find('[value="Add"]').simulate('click');
-    main.find(ButtonsComponent).find('[value="Add"]').simulate('click');
-    expect(main.state().data).toHaveLength(2);
+    expect(main.state().data).toHaveLength(3);
+
+  });
+
+  test('clicking remove button is decreasing data length by one', () => {
 
     main.find(ButtonsComponent).find('[value="Remove"]').simulate('click');
-    expect(main.state().data).toHaveLength(1);
+    expect(main.state().data).toHaveLength(2);
+
+  });
+
+  test('clicking clear button results in data length equal to zero', () => {
 
     main.find(ButtonsComponent).find('[value="Clear"]').simulate('click');
     expect(main.state().data).toHaveLength(0);
